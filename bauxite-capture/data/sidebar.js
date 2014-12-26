7632acd1ef226159373cf229968c8c15e1fba054
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2014-12-25 21:28:18 -0300

    Modest eye candy.

--- bauxite-capture/data/sidebar.js
+++ bauxite-capture/data/sidebar.js
@@ -1,3 +1,15 @@
+$(function() {
+  $('#save').click(function() {
+    addon.port.emit('store', $('#log').val());
+    addon.port.emit('save');
+  });
+  $('#exec').click(function() {
+    addon.port.emit('store', $('#log').val());
+    addon.port.emit('exec');
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
 addon.port.on('config', function(data) {
   $(function() { $('#exec').toggle(!!data.bauxite); });
 });
@@ -19,15 +31,4 @@ addon.port.on('load', function(data) {
 
 addon.port.on('detach', function() {
   addon.port.emit('store', $('#log').val());
-});
-
-$(function() {
-  $('#save').click(function() {
-    addon.port.emit('store', $('#log').val());
-    addon.port.emit('save');
-  });
-  $('#exec').click(function() {
-    addon.port.emit('store', $('#log').val());
-    addon.port.emit('exec');
-  });
 });
